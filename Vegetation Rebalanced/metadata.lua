a0b6beadbf65ee9a7cdae59e277d6ef5aaa4b37b
Tremualin <tremualin@users.noreply.github.com>
2021-05-28 03:22:38 -0400

    Vegetation Rebalanced

---
+++ Vegetation Rebalanced/metadata.lua
@@ -0,0 +1,16 @@
+return PlaceObj('ModDef', {
+	'title', "Vegetation Rebalanced",
+	'description', "ForestationPlant will now receive up to a +400% boost based on current Atmosphere, Temperature, Water, and Vegetation levels\nForestation plant will not stop contributing to global vegetation once it reaches 40%\nVegetation spreads passively based on % squared (at 10% it will be 0.01%, at 50% it will be 0.25%) even without forestation plants\n The passive spread is also boosted by Atmosphere, Temperature and Water\nNot compatible with Vegetation Goes to 11 by Choggi",
+	'id', "Tremualin_Vegetation_Rebalanced",
+	'pops_desktop_uuid', "6c2c7e13-d0a4-418d-baf9-784d03a83963",
+	'pops_any_uuid', "50d68d9d-303f-479b-8866-f8be05694c4c",
+	'author', "Tremualin",
+	'image', "Preview.png",
+	'version', 1,
+	'lua_revision', 233360,
+	'saved_with_revision', 1001586,
+	'saved', 1621651364,
+	"code", {
+		"script.lua",
+	},
+})
---
+++ Vegetation Rebalanced/script.lua
@@ -0,0 +1,75 @@
+function ForestationPlant:GetTerraformingBoostSol()
+	-- Add all of the terraforming progress so far to calculate the boost
+	local terraforming_progress_boost = GetTerraformParam("Atmosphere") + GetTerraformParam("Temperature") + GetTerraformParam("Water") + GetTerraformParam("Vegetation")
+	-- Multiply the original boost by the progress boost
+	-- Divide by const.TerraformingScale to get the percentage, then divide by 100 to get the actual boost number
+	local weightedBoost = MulDivRound(self.terraforming_boost_sol, terraforming_progress_boost, 100*const.TerraformingScale)
+  return self.terraforming_boost_sol + weightedBoost
+end
+
+-- A vegetation of 30% is now necessary to stop dust storms
+function OnMsg.ClassesPostprocess()
+	for _, preset in ipairs(Presets.TerraformingParam.Default or empty_table) do
+		if preset.id == "Vegetation" then
+	    	local newItem = PlaceObj("ThresholdItem", {"Id","DustStormStop","Threshold",30,"Hysteresis",2})
+	    	table.insert_unique(preset.Threshold, newItem)
+		end
+	end
+end
+
+function GetVegetationBoom()
+  -- Vegetation boosts itself: (vegetation%)^2, i.e: 10% would boost by 0.01% per sol
+	local vegetationSquared = MulDivRound(GetTerraformParam("Vegetation"), GetTerraformParam("Vegetation"), 100*100*const.TerraformingScale)
+
+  -- Then Water, Temperature and Atmosphere boost it even further; 50% atmosphere, water and temperature would boost it by +150%
+  local terraforming_progress_boost = GetTerraformParam("Atmosphere") + GetTerraformParam("Temperature") + GetTerraformParam("Water")
+	local weightedBoost = MulDivRound(vegetationSquared, terraforming_progress_boost, 100*const.TerraformingScale)
+	return vegetationSquared + weightedBoost
+end
+
+-- Passively boost vegetation
+GlobalVar("VegetationBoomThread", false)
+function OnMsg.LoadGame()
+	if IsValidThread(VegetationBoomThread) then 
+    DeleteThread(VegetationBoomThread)
+  end
+	VegetationBoomThread = (CreateGameTimeThread(function()
+		  local sleep = const.HourDuration / 5
+		  local sol = const.DayDuration
+		  while true do
+		    Sleep(sleep)
+		    local boom = (MulDivRound(GetVegetationBoom(), sleep, sol))
+		    print("Booming")
+		    print(boom)
+		    if 0 < boom then
+		      ChangeTerraformParam("Vegetation", boom)
+		      Msg("VegetationBoom", boom)
+		    end
+		  end
+		end))
+end
+
+-- Show the passive vegetation boost in the UI
+function OnMsg.ClassesPostprocess()
+	for _, preset in ipairs(Presets.TerraformingParam.Default or empty_table) do
+		if preset.id == "Vegetation" then
+			if not preset.Factors then 
+				preset.Factors = {}
+			end
+			local newFactor = PlaceObj("TerraformingFactorItem", {
+		      "Id",
+		      "VegetationBoom",
+		      "display_name",
+		      "Wild vegetation boom",
+		      "units",
+		      "PerSol",
+		      "GetFactorValue",
+		      function(self)
+		        return GetVegetationBoom()
+		      end
+		    })
+			table.insert_unique(preset.Factors, newFactor)
+		end
+	end
+end
+
