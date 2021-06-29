2ccb831d4c494c5d6f7af0f400ee5aee722a073e
Tremualin <tremualin@users.noreply.github.com>
2021-06-29 00:21:13 -0400

    Adding Home Gaming Mod

---
+++ Home Gaming/metadata.lua
@@ -0,0 +1,19 @@
+return PlaceObj('ModDef', {
+	'title', "Home Gaming",
+	'description', "Home Gaming adds a new upgrade to all residences (except Nurseries), a unlocked after researching Creative Realities (tech that unlocks the VR Workshop) which adds a 40% + (% of people working at Workshops) chance to automatically satisfy the Gaming interest at home. Double chances if the dome has a Network Node Spire (because faster internet)\n\nThe upgrade cost is Capacity/4 Electronics. \nComfort is equal to the residence comfort.\nGamers will recover sanity as usual.\n\nThe workshop bonus is meant to represent higher quality games developed as a result of more people pursuing vocational work.",
+	'image', "Preview.png",
+	'last_changes', "First version",
+	'id', "Tremualin_HomeGaming",
+	'steam_id', "2531242597",
+	'pops_desktop_uuid', "0d64a135-240f-415a-8496-aba480deabcc",
+	'pops_any_uuid', "41b74d44-d3b4-40f5-800a-27c7e3099c7e",
+	'author', "Tremualin",
+	'version_major', 1,
+	'version', 5,
+	'lua_revision', 233360,
+	'saved_with_revision', 1001586,
+	'code', {
+		"Code/Script.lua",
+	},
+	'saved', 1624940436,
+})
