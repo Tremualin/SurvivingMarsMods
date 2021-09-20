e104b64028e2fb75a3300e39b6dc44c30a958322
Tremualin <tremualin@users.noreply.github.com>
2021-09-20 00:17:31 -0400

    Fixed a bug with alien imprints

--- Fix Anomalies Triggering Twice/Code/Script.lua
+++ Fix Anomalies Triggering Twice/Code/Script.lua
@@ -1,7 +1,7 @@
 local orig_SubsurfaceAnomaly_ScanCompleted = SubsurfaceAnomaly.ScanCompleted
 function SubsurfaceAnomaly:ScanCompleted(scanner)
     local tech_action = self.tech_action
-    if tech_action then
+    if tech_action and tech_action ~= "aliens" then
         self.sequence = ""
     end
     orig_SubsurfaceAnomaly_ScanCompleted(self, scanner)
--- Fix Anomalies Triggering Twice/metadata.lua
+++ Fix Anomalies Triggering Twice/metadata.lua
@@ -1,18 +1,19 @@
 return PlaceObj('ModDef', {
 	'title', "Fix Anomalies Triggering Twice",
 	'description', "Fixes a bug with the Picard update which triggers Anomalies twice",
-	'last_changes', "Initial version",
+	'last_changes', "Fixed a bug with Alien Imprints",
 	'id', "Tremualin_Fix_Anomalies_Triggering_Twice",
 	'steam_id', "2596316645",
 	'pops_desktop_uuid', "a3f33d4c-cbe8-4318-a8c8-aeb30a78400d",
 	'pops_any_uuid', "3303e759-3df1-4e7a-953d-5ec63572272c",
 	'author', "Tremualin",
 	'version_major', 1,
-	'version', 4,
+	'version_minor', 1,
+	'version', 7,
 	'lua_revision', 1007000,
-	'saved_with_revision', 1007783,
+	'saved_with_revision', 1007874,
 	'code', {
 		"Code/Script.lua",
 	},
-	'saved', 1631072070,
+	'saved', 1632111400,
 })
