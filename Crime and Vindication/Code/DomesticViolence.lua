ed6bdb7e2b5423fbd555dd7351c92262cf1a8d2e
Tremualin <tremualin@users.noreply.github.com>
2022-04-07 07:41:22 -0400

    Domestic Violence chances are shown in the UI

--- Crime and Vindication/Code/DomesticViolence.lua
+++ Crime and Vindication/Code/DomesticViolence.lua
@@ -16,17 +16,13 @@ local function ReportDomesticAssault(perpetrator)
     })
 end
 
--- Domestic Violence
-local report_chances = 33
-local report_violent_chances = 11
-local orig_Colonist_DailyUpdate = Colonist.DailyUpdate
-function Colonist:DailyUpdate()
-    local unhappy = functions.IsUnhappy(self)
+function Colonist:GetTremualin_DomesticViolenceChances()
+    local chances = 0
     local traits = self.traits
     local dome = self.dome
-    orig_Colonist_DailyUpdate(self)
+    local unhappy = functions.IsUnhappy(self)
     if dome and (unhappy or traits.Renegade or traits.Violent) then
-        local chances = g_Consts.LowSanityNegativeTraitChance
+        chances = g_Consts.LowSanityNegativeTraitChance
         if traits.Violent and unhappy then
             -- Unhappy violent colonists will always commit violence
             chances = 100
@@ -36,66 +32,76 @@ function Colonist:DailyUpdate()
             local traits_by_category = functions.TraitsByCategory(traits)
             chances = chances + traits_by_category["Positive"] - traits_by_category["Negative"]
         end
+    end
+    return chances
+end
 
-        if self:Random(1, 100) <= chances then
-            local victims = {}
-            local residence = self.residence
-            if residence and residence.exclusive_trait == "Renegade" then
-                -- only find a victim if unsupervised
-                local officers_in_security_stations = functions.OfficersInSecurityStations(residence.parent_dome)
-                local renegades_in_rehabilitation = functions.RenegadesInRehabilitation(residence.parent_dome)
-                -- are there more renegades in rehabilitation than officers to monitor them?
-                if #renegades_in_rehabilitation > #officers_in_security_stations * configuration.MaxRenegadesRehabilitatedPerOfficer then
-                    victims = functions.FindAllOtherColonistsInSameResidence(self)
-                end
-            else
+-- Domestic Violence
+local report_chances = 33
+local report_violent_chances = 11
+local orig_Colonist_DailyUpdate = Colonist.DailyUpdate
+function Colonist:DailyUpdate()
+    local dome = self.dome
+    orig_Colonist_DailyUpdate(self)
+    local domesticViolenceChances = self:GetTremualin_DomesticViolenceChances()
+    if self:Random(1, 100) <= domesticViolenceChances then
+        local victims = {}
+        local residence = self.residence
+        if residence and residence.exclusive_trait == "Renegade" then
+            -- only find a victim if unsupervised
+            local officers_in_security_stations = functions.OfficersInSecurityStations(residence.parent_dome)
+            local renegades_in_rehabilitation = functions.RenegadesInRehabilitation(residence.parent_dome)
+            -- are there more renegades in rehabilitation than officers to monitor them?
+            if #renegades_in_rehabilitation > #officers_in_security_stations * configuration.MaxRenegadesRehabilitatedPerOfficer then
                 victims = functions.FindAllOtherColonistsInSameResidence(self)
             end
+        else
+            victims = functions.FindAllOtherColonistsInSameResidence(self)
+        end
 
-            if #victims > 0 then
-                local victim = table.rand(victims)
+        if #victims > 0 then
+            local victim = table.rand(victims)
 
-                -- collect statistics
-                dome.Tremualin_DomesticViolenceAssaults = dome.Tremualin_DomesticViolenceAssaults or 0
-                dome.Tremualin_DomesticViolenceAssaults = dome.Tremualin_DomesticViolenceAssaults + 1
+            -- collect statistics
+            dome.Tremualin_DomesticViolenceAssaults = dome.Tremualin_DomesticViolenceAssaults or 0
+            dome.Tremualin_DomesticViolenceAssaults = dome.Tremualin_DomesticViolenceAssaults + 1
 
-                -- apply damage, reduced by security stations
-                local damageDecrease = dome:GetSecurityStationDamageDecrease()
-                local random_health_damage = -MulDivRound(self:Random(1, 15), const.Scale.Stat * (100 - damageDecrease), 100)
-                local random_sanity_damage = -MulDivRound(self:Random(1, 15), const.Scale.Stat * (100 - damageDecrease), 100)
-                victim:ChangeHealth(random_health_damage, "Domestic violence assault ")
-                victim:ChangeSanity(random_sanity_damage, "Domestic violence assault ")
+            -- apply damage, reduced by security stations
+            local damageDecrease = dome:GetSecurityStationDamageDecrease()
+            local random_health_damage = -MulDivRound(self:Random(1, 15), const.Scale.Stat * (100 - damageDecrease), 100)
+            local random_sanity_damage = -MulDivRound(self:Random(1, 15), const.Scale.Stat * (100 - damageDecrease), 100)
+            victim:ChangeHealth(random_health_damage, "Domestic violence assault ")
+            victim:ChangeSanity(random_sanity_damage, "Domestic violence assault ")
 
-                if victim.traits.Child then
-                    -- child will have permanent scars
-                    victim.domestic_violence = true
-                elseif self:Random(1, 100) <= g_Consts.LowSanityNegativeTraitChance then
-                    functions.AddSanityBreakdownFlaw(victim)
+            if victim.traits.Child then
+                -- child will have permanent scars
+                victim.domestic_violence = true
+            elseif self:Random(1, 100) <= g_Consts.LowSanityNegativeTraitChance then
+                functions.AddSanityBreakdownFlaw(victim)
+            end
+            if self.traits.Child then
+                -- violence is carried over to adulthood
+                self.domestic_violence = true
+            end
+            -- Violent victims retaliate
+            if victim.traits.Violent then
+                dome.Tremualin_DomesticViolenceRetalations = dome.Tremualin_DomesticViolenceRetalations or 0
+                dome.Tremualin_DomesticViolenceRetalations = dome.Tremualin_DomesticViolenceRetalations + 1
+                self:ChangeHealth(random_health_damage, "Domestic violence retaliation ")
+                self:ChangeSanity(random_sanity_damage, "Domestic violence retaliation ")
+                if self:Random(1, 100) <= g_Consts.LowSanityNegativeTraitChance then
+                    functions.AddSanityBreakdownFlaw(self)
                 end
-                if self.traits.Child then
-                    -- violence is carried over to adulthood
-                    self.domestic_violence = true
+                -- report the victim's retaliation
+                if self:Random(1, 100) <= report_violent_chances then
+                    ReportDomesticAssault(victim)
                 end
-                -- Violent victims retaliate
-                if victim.traits.Violent then
-                    dome.Tremualin_DomesticViolenceRetalations = dome.Tremualin_DomesticViolenceRetalations or 0
-                    dome.Tremualin_DomesticViolenceRetalations = dome.Tremualin_DomesticViolenceRetalations + 1
-                    self:ChangeHealth(random_health_damage, "Domestic violence retaliation ")
-                    self:ChangeSanity(random_sanity_damage, "Domestic violence retaliation ")
-                    if self:Random(1, 100) <= g_Consts.LowSanityNegativeTraitChance then
-                        functions.AddSanityBreakdownFlaw(self)
-                    end
-                    -- report the victim's retaliation
-                    if self:Random(1, 100) <= report_violent_chances then
-                        ReportDomesticAssault(victim)
-                    end
-                end
-                -- 33% chance of being reported and becoming a Renegade
-                if not self.traits.Renegade and not self.traits.Child then
-                    -- Violent people have lower chances of being reported to authorities
-                    if (self.traits.Violent and self:Random(1, 100) <= report_violent_chances) or (self:Random(1, 100) <= report_chances) then
-                        ReportDomesticAssault(self)
-                    end
+            end
+            -- 33% chance of being reported and becoming a Renegade
+            if not self.traits.Renegade and not self.traits.Child then
+                -- Violent people have lower chances of being reported to authorities
+                if (self.traits.Violent and self:Random(1, 100) <= report_violent_chances) or (self:Random(1, 100) <= report_chances) then
+                    ReportDomesticAssault(self)
                 end
             end
         end
@@ -122,6 +128,11 @@ local function SetViolentUIWarning()
     if ColonistTraitsWarnings then
         ColonistTraitsWarnings["Violent"] = Untranslated("This Colonist is Violent. Violent colonists will commit domestic violence, unless rehabilitated at a Sanatorium.")
     end
+end
+
+local Tremualin_Orig_Colonist_GetUIInfo = Colonist.GetUIInfo
+function Colonist:GetUIInfo(remove_descr)
+    return Tremualin_Orig_Colonist_GetUIInfo(self, remove_descr) .. "<newline><left>" .. Untranslated("Chances of committing violence<right><Tremualin_DomesticViolenceChances>%", self)
 end
 
 function OnMsg.ClassesGenerate()
