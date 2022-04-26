8c35a453d5556aad49322912c2d20fa40e33c0cb
Tremualin <tremualin@users.noreply.github.com>
2022-04-26 00:35:39 -0400

    Advanced Terraforming - initial layout

---
+++ Advanced Terraforming/items.lua
@@ -0,0 +1,30 @@
+return {
+PlaceObj('ModItemCode', {
+	'name', "Atmosphere",
+	'FileName', "Code/Atmosphere.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "Miscellaneous",
+	'FileName', "Code/Miscellaneous.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "Disasters",
+	'FileName', "Code/Disasters.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "SpaceRaceIntegration",
+	'FileName', "Code/SpaceRaceIntegration.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "Temperature",
+	'FileName', "Code/Temperature.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "Vegetation",
+	'FileName', "Code/Vegetation.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "Water",
+	'FileName', "Code/Water.lua",
+}),
+}
---
+++ Advanced Terraforming/metadata.lua
@@ -0,0 +1,20 @@
+return PlaceObj('ModDef', {
+    'title', "Advanced Terraforming",
+    'id', "Tremualin_AdvancedTerraforming",
+    'pops_desktop_uuid', "a1dc9b5f-4fd9-409c-99c4-1a3dfc3eda7d",
+    'pops_any_uuid', "2d2a040a-197f-48d6-8ec4-200d01844f0b",
+    'author', "Tremualin",
+    'version', 8,
+    'lua_revision', 1009413,
+    'saved_with_revision', 1010838,
+    'code', {
+        "Code/Atmosphere.lua",
+        "Code/Miscellaneous.lua",
+        "Code/Disasters.lua",
+        "Code/SpaceRaceIntegration.lua",
+        "Code/Temperature.lua",
+        "Code/Vegetation.lua",
+        "Code/Water.lua",
+    },
+    'saved', 1650938202,
+})
