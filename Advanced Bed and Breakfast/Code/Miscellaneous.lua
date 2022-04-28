d911cf3d15b02650994cee3ab25db29d14f3ef49
Tremualin <tremualin@users.noreply.github.com>
2022-04-27 20:47:40 -0400

    Updated Signal Boosters description

--- Advanced Bed and Breakfast/Code/Miscellaneous.lua
+++ Advanced Bed and Breakfast/Code/Miscellaneous.lua
@@ -26,7 +26,7 @@ local function AdvancedSignalBoosters()
             Label = "RCRoverAndChildren",
             Prop = "work_radius"
         })
-        --tech.description = Untranslated("Asteroid Lander Upgrade (<em>Solar Energy Landers</em>) - ") .. SOLAR_ENERGY_LANDER_DESCRIPTION .. "\n" .. tech.description
+        tech.description = Untranslated("Increases the service range on <em>all drone controllers</em> by <param1> hexes.\n\n<grey>\226\128\156In the new era, thought itself will be transmitted by radio.\226\128\157\n<right>Guglielmo Marconi</grey><left>"),
         table.insert(tech, #tech + 1, modifyAllRocketsEffect)
         table.insert(tech, #tech + 1, modifyAllElevatorsEffect)
         table.insert(tech, #tech + 1, modifyAllRCCommandersEffect)
