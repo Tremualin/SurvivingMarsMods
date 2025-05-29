d975a94c82668122d3b4909fc8fa75e68d0f2431
Tremualin <tremualin@users.noreply.github.com>
2025-05-29 00:56:25 -0400

    Quirky Space Y: Less angsty descriptions

--- Quirky Sponsors SpaceY/Code/Script.lua
+++ Quirky Sponsors SpaceY/Code/Script.lua
@@ -1,114 +1,120 @@
 local AddOnScreenNotification = AddOnScreenNotification
 
-local SpaceY_Space_Race_Description = [[Research per Sol: <research(SponsorResearch)>
+local SpaceY_Space_Race_Description = [[
+Research per Sol: <research(SponsorResearch)>
 Rare Metals price: $<ExportPricePreciousMetals> M
  
 <green>Dragon Rocket</green> - has smaller cargo capacity (<cargo>) but is faster and requires less fuel
-<green>Solar Array</green> - a large cluster of solar panels that have lower maintenance cost but are vulnerable to dust storms
+<green>Solar Array</green> - a large cluster of solar panels with higher production and lower maintenance cost, but vulnerable to dust storms
 <green>Just Read the Instructions</green> - Drone Hubs start with additional Drones
 <green>Governmental Supply Contracts</green> - 50% cheaper advanced resources
-<green>Imperator of Mirihi</green> - Begin the game with <em>Interplanetary Learning</em>
-<white>Acquirer of Companies</white> - Every 20th Sol, randomly gain or lose 500M
-<white>Father of Mars</green> - Begin the game with <em>Cloning</em>. However, Colonists won't have Children
-<red>Breaker of Unions</red> - <em>Heavy Workload</em> is mandatory and cannot be turned off]]
+ 
+<em>Quirks:</em>
+<green>Targeted Recruiting</green> - All applicants (except Tourists) are <em>Workaholic</em>
+<green>Vocation Oriented</green> - Begin the game with <em>Vocation-Oriented Society</em>
+<red>Unrealistic Deadlines</red> - Lose <em>$500M</em> any time a <em>Rival</em> obtains a <em>Milestone</em>
+<red>Work-Work Balance</red> - <em>Birth Rate</em> halved
+<red>Sixteen Tons</red> - <em>Heavy Workload</em> is always on
+]]
 
-local SpaceY_No_Space_Race_Description = [[Research per Sol: <research(SponsorResearch)>
+local SpaceY_No_Space_Race_Description = [[
+Research per Sol: <research(SponsorResearch)>
 Rare Metals price: $<ExportPricePreciousMetals> M
  
 <green>Just Read the Instructions</green> - Drone Hubs start with additional Drones
 <green>Governmental Supply Contracts</green> - 50% cheaper advanced resources
-<green>Imperator of Mirihi</green> - Begin the game with <em>Interplanetary Learning</em>
-<white>Acquirer of Companies</white> - Every 20th Sol, randomly gain or lose 500M.
-<white>Father of Mars</green> - Begin the game with <em>Cloning</em>. However, Colonists won't have Children
-<red>Breaker of Unions</red> - <em>Heavy Workload</em> is mandatory and cannot be turned off]]
+ 
+<em>Quirks:</em>
+<green>Targeted Recruiting</green> - All applicants (except Tourists) are <em>Workaholic</em>
+<green>Vocation Oriented</green> - Begin the game with <em>Vocation-Oriented Society</em>
+<red>Work-Work Balance</red> - <em>Birth Rate</em> halved
+<red>Sixteen Tons</red> - <em>Heavy Workload</em> is always on
+]]
 
-local EVERY_X_SOLS = 20
-local POSITIVE_FUNDING_FROM_COMPANIES = 500000000
-local NEGATIVE_FUNDING_FROM_COMPANIES = -500000000
-GlobalVar("Tremualin_SpaceY_AcquirerOfCompaniesThread", false)
-function Tremualin_SpaceY_AcquirerOfCompanies(...)
-    if not IsValid(Tremualin_SpaceY_AcquirerOfCompaniesThread) then
-        DeleteThread(Tremualin_SpaceY_AcquirerOfCompaniesThread)
+local function IsSpaceYSponsor()
+    return g_CurrentMissionParams.idMissionSponsor == "SpaceY"
+end
+
+-- Unrealistic Deadlines
+function OnMsg.RivalMilestone(...)
+    if IsSpaceYSponsor() then
+        UIColony.funds.funding = UIColony.funds.funding - 500000000
+        AddOnScreenNotification("Tremualin_QuirkySpaceY_UnrealisticDeadlines", false)
     end
-    Tremualin_SpaceY_AcquirerOfCompaniesThread = CreateGameTimeThread(function()
-        while true do
-            local funding = POSITIVE_FUNDING_FROM_COMPANIES
-            if Random(2) == 0 then
-                funding = NEGATIVE_FUNDING_FROM_COMPANIES
-            end
-            local ok, day = WaitMsg("NewDay")
-            if ok and day and day % EVERY_X_SOLS == 0 then
-                UIColony.funds:ChangeFunding(funding)
-                if funding > 0 then
-                    AddOnScreenNotification("Tremualin_SpaceY_CompanySold", nil, {
-                        funding = funding
-                    }, nil, city.map_id)
-                else
-                    AddOnScreenNotification("Tremualin_SpaceY_CompanyBought", nil, {
-                        funding = funding
-                    }, nil, city.map_id)
-                end
-            end
-        end
-    end)
 end
 
 function OnMsg.ClassesPostprocess()
-    if OnScreenNotificationPresets.Tremualin_SpaceY_CompanySold then
+    if OnScreenNotificationPresets.Tremualin_QuirkySpaceY_UnrealisticDeadlines then
         return
     end
-    PlaceObj("OnScreenNotificationPreset", {
-        group = "Default",
-        id = "Tremualin_SpaceY_CompanySold",
-        image = "UI/Icons/Notifications/New/funding.tga",
-        text = Untranslated("Funding gained: <funding>"),
-    title = Untranslated("Company Sold")})
-end
 
-function OnMsg.ClassesPostprocess()
-    if OnScreenNotificationPresets.Tremualin_SpaceY_CompanyBought then
-        return
-    end
     PlaceObj("OnScreenNotificationPreset", {
+        ImagePreview = "UI/Icons/Notifications/New/funding_2.tga",
+        SortKey = 1000200,
+        VignetteImage = "UI/Onscreen/onscreen_gradient_red.tga",
+        VignettePulseDuration = 2000,
+        close_on_read = true,
+        expiration = 150000,
+        fx_action = "UINotificationResource",
         group = "Default",
-        id = "Tremualin_SpaceY_CompanyBought",
+        id = "Tremualin_QuirkySpaceY_UnrealisticDeadlines",
         image = "UI/Icons/Notifications/New/funding_2.tga",
-        text = Untranslated("Funding lost: <funding>"),
-    title = Untranslated("Company Bought")})
+        priority = "Important",
+        text = Untranslated("We have lost $500M for failing to accomplish a milestone before a rival"),
+    title = Untranslated("Unmet deadlines")})
 end
 
-function OnMsg.ClassesPostprocess()
-    local SpaceY = Presets.MissionSponsorPreset[1].SpaceY
-    if not SpaceY.Tremualin_QuirkySponsor then
-        table.insert(SpaceY, PlaceObj('Effect_ModifyLabel', {
+local function QuirkySponsorPreset()
+    local spaceY = Presets.MissionSponsorPreset[1].SpaceY
+    -- Don't modify the sponsor twice
+    if not spaceY.Tremualin_QuirkySponsor then
+        -- Work-Work Balance - Birth rate is halved
+        table.insert(spaceY, PlaceObj('Effect_ModifyLabel', {
             Label = "Consts",
-            Percent = 5000,
-            Prop = "MinComfortBirth",
+            Percent = 50,
+            Prop = "BirthThreshold",
         }))
 
-        table.insert(SpaceY, PlaceObj('Effect_GrantTech', {Field = "Breakthroughs", Research = "Cloning"}))
+        -- Vocation Oriented
+        table.insert(spaceY, PlaceObj('Effect_GrantTech', {Field = "Breakthroughs", Research = "Vocation-Oriented Society"}))
 
-        table.insert(SpaceY, PlaceObj('Effect_GrantTech', {Field = "Breakthroughs", Research = "InterplanetaryLearning"}))
-
-        table.insert(SpaceY, PlaceObj('Effect_Code', {OnApplyEffect = Tremualin_SpaceY_AcquirerOfCompanies}))
-
-        -- If Space Race; add the unique buildings description
+        -- If Space Race is installed; add the unique buildings description
         local SpaceY_Description = SpaceY_No_Space_Race_Description
         if IsDlcAvailable("gagarin") then
             SpaceY_Description = Untranslated(SpaceY_Space_Race_Description)
         end
-        SpaceY.effect = SpaceY_Description
-        SpaceY.challenge_mod = 170
-        SpaceY.difficulty = T(574527829385, "Hard")
-        SpaceY.Tremualin_QuirkySponsor = true
+        spaceY.effect = SpaceY_Description
+
+        -- Set the sponsor difficulty to hard and update the challenge rating
+        spaceY.challenge_mod = 170
+        spaceY.difficulty = T(574527829385, "Hard")
+
+        -- Remember that we've already modified this sponsor so we don't do it again
+        spaceY.Tremualin_QuirkySponsor = true
     end
 end
 
-local function MandatoryOvertimeIfSpaceY()
-    local function IsSpaceYSponsor()
-        return g_CurrentMissionParams.idMissionSponsor == "SpaceY"
-    end
+OnMsg.ClassesPostprocess = QuirkySponsorPreset
 
+-- Begin Lake Changes
+function ImproveSolarArrays()
+    if IsDlcAvailable("gagarin") then
+        local templateNames = {"SolarArray"}
+        local templates = {}
+        for _, templateName in pairs(templateNames) do
+            table.insert(templates, BuildingTemplates[templateName])
+            table.insert(templates, ClassTemplates.Building[templateName])
+        end
+        for _, template in ipairs(templates) do
+            template.electricity_production = 85000
+        end
+    end
+end
+
+OnMsg.ClassesPostprocess = ImproveSolarArrays
+
+-- Sixteen Tons - SpaceY colonists always work overtime. Always
+local function MandatoryOvertimeIfSpaceY()
     local Tremualin_Orig_ToggleOvertime = Workplace.ToggleOvertime
     function Workplace:ToggleOvertime(...)
         if not IsSpaceYSponsor() then
@@ -127,4 +133,17 @@ local function MandatoryOvertimeIfSpaceY()
     end
 end
 
-OnMsg.ClassesGenerate = MandatoryOvertimeIfSpaceY
+-- Targeted Recruiting
+local function WorkaholicSpaceXApplicants()
+    local Tremualin_Orig_GenerateApplicant = GenerateApplicant
+    function GenerateApplicant(time, city)
+        local colonist = Tremualin_Orig_GenerateApplicant(time, city)
+        if IsSpaceYSponsor() and not colonist.Tourist then
+            colonist.traits["Hippie"] = nil
+            colonist.traits["Workaholic"] = true
+        end
+        return colonist
+    end
+end
+
+OnMsg.ClassesGenerate = WorkaholicSpaceXApplicants()
