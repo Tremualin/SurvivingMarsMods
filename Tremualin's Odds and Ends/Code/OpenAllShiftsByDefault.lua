a412844cdd8fe6e4a9fb0bbb45474941c77cdbbb
Tremualin <tremualin@users.noreply.github.com>
2025-06-18 15:38:53 -0400

    Odds and Ends: Open all shifts for new buildings

---
+++ Tremualin's Odds and Ends/Code/OpenAllShiftsByDefault.lua
@@ -0,0 +1,10 @@
+-- Enable all shifts for all buildings, regardless of what the game thinks
+function OnMsg.ClassesPreprocess()
+    local buildingTemplates = BuildingTemplates
+    for id, bt in pairs(buildingTemplates) do
+        if bt.enabled_shift_1 or bt.enabled_shift_2 then
+            bt.enabled_shift_1 = true
+            bt.enabled_shift_3 = true
+        end
+    end -- for id, buildingTemplate
+end -- function OnMsg.ClassesPostprocess
