d94eb07cbf343679679458a46f10ef402ac092f0
Tremualin <tremualin@users.noreply.github.com>
2025-06-27 08:36:29 -0400

    OandE: Royals GR. Colonists stats are green if over 90, and red if below 50.

---
+++ Tremualin's Odds and Ends/Code/Royals.lua
@@ -0,0 +1,22 @@
+function OnMsg.ClassesPostprocess()
+    PlaceObj("GameRules", {
+        SortKey = 200001,
+        challenge_mod = 40,
+        description = Untranslated("Colonists stats are green if above 90(rather than 70) and low if below 50(rather than 30)"),
+        display_name = Untranslated("Royals"),
+        flavor = Untranslated("<grey>\"Cristal, Maybach, diamonds on your timepiece. Jet planes, islands, tiger's on a gold leash\"<newline><right>Lorde</grey><left>"),
+        group = "Default",
+        id = "Tremualin_Royals",
+        PlaceObj("Effect_ModifyLabel", {
+            Label = "Consts",
+            Amount = 20000,
+            Prop = "LowStatLevel"
+        }),
+        PlaceObj("Effect_ModifyLabel", {
+            Label = "Consts",
+            Amount = 20000,
+            Prop = "HighStatLevel"
+        }),
+    })
+end
+
